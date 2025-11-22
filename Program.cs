8515df116510350e983949ff899ae0eda28b6c1c
yaliitach2010-wq <yaliitach2010@gmail.com>
2025-11-22 21:41:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int num=10;num<=99;num++)
+                
+            {
+                int a = num / 10;
+                int b = num % 10;
+
+                int number = num;
+                int p = a + b;
+                int q = a * b;
+
+                if (number - q == 4 * p)
+                {
+                    Console.WriteLine("the number is:" + num);
+
+                }
+
+            }
+        }
+    }
+}
