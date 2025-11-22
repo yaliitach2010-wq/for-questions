c6a92ae3691126d0bbf74f6d227da150adc479e9
yaliitach2010-wq <yaliitach2010@gmail.com>
2025-11-22 21:46:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,29 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace for_5
+namespace for_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            for(int num=10;num<=99;num++)
-                
+            double sum = 0;
+
+            for(int n=13;n<=99;n++)
             {
-                int a = num / 10;
-                int b = num % 10;
-
-                int number = num;
-                int p = a + b;
-                int q = a * b;
-
-                if (number - q == 4 * p)
-                {
-                    Console.WriteLine("the number is:" + num);
-
-                }
+             sum+=1.0/(n*(n+1));
 
             }
+            Console.WriteLine("the enswer is:"+sum);
         }
     }
 }
